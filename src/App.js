db9cda2a560fd0e5f8a8d0bb6a2ac4050ed39d3f
Md Habibur Rahman <typetohabib@gmail.com>
2022-11-05 22:23:14 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Data from'./Data'
+
+function App() {
+  return (
+    <div id="app">
+      <Data/>
+    </div>
+  );
+}
+
+export default App;
